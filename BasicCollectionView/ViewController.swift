3ba9ab9258d0b91aec79d16dea31a43080bb7da9
Salvador <Sagarcia2713@gmail.com>
2021-11-14 14:26:53 -0600

    Initial Commit

---
+++ BasicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by Salvador Garcia on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
